a48ebf9f554d7d7ea96f338b07ee6677539d881b
dt204684 <dt204684@HDDT0129.ad.dstsystems.com>
2020-03-12 12:46:40 +0530

    This is git pull reguest demo by test engineer

---
+++ src/com/qa/test/Demo2.java
@@ -0,0 +1,10 @@
+package com.qa.test;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is git pull requset demo project by test engineer...");
+	}
+
+}

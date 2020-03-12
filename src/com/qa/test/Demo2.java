f15d89ebc65d7c145874830c7852775408b1bae0
dt204684 <dt204684@HDDT0129.ad.dstsystems.com>
2020-03-12 14:19:54 +0530

    Added the check

--- src/com/qa/test/Demo2.java
+++ src/com/qa/test/Demo2.java
@@ -5,6 +5,8 @@ public class Demo2 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is git pull requset demo project by test engineer...");
+		
+		System.out.println("Added the additional check");
 	}
 
 }
